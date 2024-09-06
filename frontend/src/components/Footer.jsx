9d32ce9a16adaf39493a39730394aa80ed3f981f
Vinilazza <108952051+Vinilazza@users.noreply.github.com>
2024-09-06 17:25:34 -0300

    FrontendCss

---
+++ frontend/src/components/Footer.jsx
@@ -0,0 +1,13 @@
+import './Footer.css'
+
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <footer className="footer">
+        <p>ReactGram &copy; 2024:</p>
+    </footer>
+  )
+}
+
+export default Footer
---
+++ frontend/src/pages/Auth/Login.jsx
@@ -0,0 +1,9 @@
+import './Auth.css'
+
+const Login = () => {
+  return (
+    <div>login</div>
+  )
+}
+
+export default Login
